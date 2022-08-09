372b53a9c3b31f76580ddc253a496fa4a44b2fbb
bhavyaprasad <87380192+bhavyaprasad@users.noreply.github.com>
2022-08-09 14:26:38 +0530

    Update table.js

--- src/components/table.js
+++ src/components/table.js
@@ -1,105 +1,177 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
 import BootstrapTable from "react-bootstrap-table-next";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
-import "./styles.css";
+import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 
-// const navigate = useNavigate();
-const bonds = [
-  {
-    bond_id: 1,
-    issuer_name: "Item 1",
-    trade_date: "abc-123",
-    maturity_date: "xyz=123",
-    bond_type: "daksh",
-    status: "active",
-    update: "google.com"
-  },
-  {
-    bond_id: 2,
-    issuer_name: "Item 2",
-    trade_date: "abc-123",
-    maturity_date: "xyz=123",
-    bond_type: "daksh",
-    status: "inactive",
-    update: "google.com"
-  }
-];
+const { SearchBar } = Search;
 
-export default class Table extends Component {
-  constructor() {
-    super();
-    this.state = {
-      // For displaying data
-      columns: [
-        {
-          dataField: "bond_id",
-          text: "Bond Id",
-          sort: true
-        },
-        {
-          dataField: "issuer_name",
-          text: "Issuer Name",
-          sort: true
-        },
-        {
-          dataField: "trade_date",
-          text: "Trade Date",
-          sort: true
-        },
-        {
-          dataField: "maturity_date",
-          text: "Maturity Date",
-          sort: true
-        },
-        {
-          dataField: "bond_type",
-          text: "Bond Type",
-          sort: true
-        },
-        {
-          dataField: "status",
-          text: "Status",
-          sort: true
-        },
-        {
-          dataField: "follow",
-          text: "Follow",
-          formatter: this.linkFollow,
-          sort: true
-        }
-      ],
-      isFollow: true
-    };
+let nameFilter;
+let priceFilter;
+let stockFilter;
+let originFilter;
 
-    this.onFollowChanged.bind(this);
-  }
-
-  onFollowChanged() {
-    this.setState({ isFollow: !this.state.isFollow });
-    console.log(this.state.isFollow);
-  }
-
-  linkFollow = (cell, row, rowIndex, formatExtraData) => {
-    return <Button onClick={() => alert("google.com")}>Update</Button>;
+const ClearButton = props => {
+  const handleClick = () => {
+    props.onSearch("");
+    props.clearAllFilter();
   };
+  return (
+    <Button
+      variant="secondary"
+      onClick={handleClick}
+      style={{
+        fontSize: "16px",
+        padding: "5px",
+        margin: "10px",
+        height: "40px"
+      }}
+    >
+      Clear
+    </Button>
+  );
+};
+
+class Table extends React.Component {
+  columns = [
+    {
+      dataField: "bond_id",
+      text: "Bond Id",
+      filter: textFilter({
+        getFilter: filter => {
+          nameFilter = filter;
+        }
+      })
+    },
+    {
+      dataField: "issuer_name",
+      text: "Issuer Name",
+      filter: textFilter({
+        getFilter: filter => {
+          priceFilter = filter;
+        }
+      }),
+      sort: true
+    },
+    {
+      dataField: "trade_date",
+      text: "Trade Date",
+      filter: textFilter({
+        getFilter: filter => {
+          stockFilter = filter;
+        }
+      })
+    },
+    {
+      dataField: "maturity_date",
+      text: "Maturity Date",
+      filter: textFilter({
+        getFilter: filter => {
+          originFilter = filter;
+        }
+      })
+    },
+    {
+      dataField: "bond_type",
+      text: "Bond Type",
+      filter: textFilter({
+        getFilter: filter => {
+          originFilter = filter;
+        }
+      })
+    },
+    {
+      dataField: "status",
+      text: "Status",
+      filter: textFilter({
+        getFilter: filter => {
+          originFilter = filter;
+        }
+      })
+    },
+    {
+      dataField: "update",
+      text: "Update",
+      filter: textFilter({
+        getFilter: filter => {
+          originFilter = filter;
+        }
+      })
+    }
+  ];
+
+  clearAllFilter() {
+    nameFilter("");
+    priceFilter("");
+    originFilter("");
+    stockFilter("");
+  }
+
+  products = [
+    {
+      bond_id: 1,
+      issuer_name: "Item 1",
+      trade_date: "abc-123",
+      maturity_date: "xyz=123",
+      bond_type: "daksh",
+      status: "active",
+      update: <button onClick = {() => alert("Form here")}>Update</button>
+    },
+    {
+      bond_id: 2,
+      issuer_name: "Item 2",
+      trade_date: "abc-123",
+      maturity_date: "xyz=123",
+      bond_type: "daksh",
+      status: "inactive",
+      update: <button onClick = {() => alert("Form here")}>Update</button> 
+    },
+    {
+      bond_id: 3,
+      issuer_name: "Item 3",
+      trade_date: "abc-123",
+      maturity_date: "xyz=123",
+      bond_type: "daksh",
+      status: "active",
+      update: <button onClick = {() => alert("Form here")}>Update</button>
+    }
+  ];
 
   render() {
     return (
-      <div style={{ padding: "20px" }}>
-        <h1 className="h2">Bonds</h1>
-        <BootstrapTable
-          keyField="id"
-          data={bonds}
-          columns={this.state.columns}
-        />
+      <div>
+        <h1>Clear search bar and filter</h1>
+        <ToolkitProvider
+          bootstrap4
+          keyField="name"
+          data={this.products}
+          columns={this.columns}
+          search
+        >
+          {props => (
+            <div>
+              <SearchBar
+                {...props.searchProps}
+                style={{ width: "400px", height: "40px" }}
+              />
+              <ClearButton
+                {...props.searchProps}
+                clearAllFilter={this.clearAllFilter}
+              />
+              <BootstrapTable
+                {...props.baseProps}
+                filter={filterFactory()}
+                noDataIndication="There is no solution"
+                striped
+                hover
+                condensed
+              />
+            </div>
+          )}
+        </ToolkitProvider>
       </div>
     );
   }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Table />, rootElement);
+export default Table;
